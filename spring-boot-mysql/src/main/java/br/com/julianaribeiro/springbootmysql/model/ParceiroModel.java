d91a013ee020f9c6699ebd3dcfcfa6615dc71d11
JuRibeiro <ju.ribeiro1208@gmail.com>
2021-09-19 21:34:46 -0300

    iniciando models e repository

---
+++ spring-boot-mysql/src/main/java/br/com/julianaribeiro/springbootmysql/model/ParceiroModel.java
@@ -0,0 +1,95 @@
+package br.com.julianaribeiro.springbootmysql.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table (name = "tb_parceiro")
+public class ParceiroModel {
+
+	//atributos
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank
+	private String tradingName;
+	
+	@NotBlank
+	private String ownerName;
+	
+	@NotBlank
+	private String documento;
+	
+	@NotBlank
+	private String endereco;
+
+	//encapsulamento
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTradingName() {
+		return tradingName;
+	}
+
+	public void setTradingName(String tradingName) {
+		this.tradingName = tradingName;
+	}
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	//metodos hashcode
+	@Override
+	public int hashCode() {
+		return getClass().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ParceiroModel other = (ParceiroModel) obj;
+		return id == other.id;
+	}
+	
+	
+	
+	
+}
+
